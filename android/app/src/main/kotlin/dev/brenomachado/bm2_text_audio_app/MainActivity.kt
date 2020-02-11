74b25cd025c5cea920f7afde4704dfb8d63c01f8
breno.monte <breno.monte@unimedteresina.com.br>
2020-02-11 16:57:13 -0300

    Once upon a time...

---
+++ android/app/src/main/kotlin/dev/brenomachado/bm2_text_audio_app/MainActivity.kt
@@ -0,0 +1,12 @@
+package dev.brenomachado.bm2_text_audio_app
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
