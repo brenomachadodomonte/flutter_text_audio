43dc0508b83c80f004ae2efcfbcf392b48a0c70a
breno.monte <breno.monte@unimedteresina.com.br>
2020-02-12 15:14:22 -0300

    Listing audios in Home screen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:bm2_text_audio_app/screens/audio.dart';
+import 'package:bm2_text_audio_app/data/data.dart';
 
 class Home extends StatelessWidget {
+
+  Widget _getTile(Map audio, BuildContext context){
+    return InkWell(
+      onTap: (){
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => Audio(audio: audio),
+            ));
+      },
+      child: ListTile(
+        title: Text(audio['title']),
+        subtitle: Text(audio['subtitle']),
+        trailing: Icon(Icons.arrow_forward_ios),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -8,7 +28,14 @@ class Home extends StatelessWidget {
         title: Text('Texts & Audios'),
         centerTitle: true,
       ),
-      body: Container(),
+      body: Container(
+        child: ListView(
+          children: ListTile.divideTiles(
+              context: context,
+              tiles: dataAudios.map((audio) => _getTile(audio, context)).toList()
+          ).toList(),
+        ),
+      ),
     );
   }
 }
