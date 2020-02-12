4125652d10c2305378d6e5d0c53e6f7d2cbc1a12
breno.monte <breno.monte@unimedteresina.com.br>
2020-02-12 14:07:50 -0300

    Create some data and imports audios

---
+++ lib/data/data.dart
@@ -0,0 +1,16 @@
+
+
+List dataAudios = [
+  {
+    'id':1,
+    'name': 'Jack Hannaford - 01',
+    'en_text': 'There was once a farmer and his wife. She had been married before, her first husband had died, and now she was married again. They lived on a remote farm in the west of England, and what a pair of fools they were! Which of them was the most foolish? Listen to the story and decide for yourself. In those days, there also lived an old soldier called Jack Hannaford. His coat was old and he was poor, but nobody thought that Jack Hannaford was stupid. He was sly like a fox. When he left the army, he walked all around the country, looking for ways to play his tricks. After he had traveled for some time, he came across the farm belonging to the pair. He knocked on the door of the house, and a moment later, the wife answered. She looked him up and down, quite astonished, because few strangers were able to make the difficult journey to their home. “Where did you come from?” she asked.',
+    'pt_text': 'Era uma vez um fazendeiro e sua esposa. Ela tinha sido casada antes, seu primeiro marido tinha morrido, e agora ela era casada novamente. Eles moravam em uma remota fazenda no oeste da Inglaterra, e que par de tolos eles eram! Qual deles era o mais tolo? Escute a história e decida você mesmo. Naqueles dias, existia também um velho soldado chamado Jack Hannaford. Seu casaco era velho e ele era pobre, mas ninguém pensava que Jack Hannaford era bobo. Ele era astuto como uma raposa. Quando ele deixou o exército, ele caminhou por todo o país, procurando por maneiras de fazer suas artimanhas. Depois de ter viajado por algum tempo, ele deu de cara com a fazenda que pertencia ao casal. Ele bateu na porta da casa e pouco depois a esposa respondeu. Ela o olhou de cima a baixo, muito surpresa, porque poucos desconhecidos era capazes de fazer a difícil jornada até a casa deles. “De onde você veio?”, ela perguntou.',
+    'audios': [
+      {'name': 'Jake','path': 'assets/audios/1/jake.mp3'},
+      {'name': 'John','path': 'assets/audios/1/john.mp3'},
+      {'name': 'Moira','path': 'assets/audios/1/moira.mp3'},
+      {'name': 'Natalie','path': 'assets/audios/1/natalie.mp3'},
+    ]
+  },
+];
