123586d3bc9e56bc131cd7dc8108b4669aa1b046
breno.monte <breno.monte@unimedteresina.com.br>
2020-02-21 09:03:36 -0300

    Adding some texts and audios

--- lib/data/data.dart
+++ lib/data/data.dart
@@ -15,7 +15,7 @@ List dataAudios = [
     ]
   },
   {
-    'id':1,
+    'id':2,
     'title': 'Jack Hannaford',
     'subtitle': 'Parte 02',
     'en_text': 'Jack looked up at the pale blue sky, and he said, “I came from Heaven.” “My goodness!”, she said. “Did you see my husband there?” (The farmer’s wife was talking about her first husband, the man who had died). “Oh yes, I got to know him well when I was staying in Heaven,” said Jack. “And how is he doing?”, asked the woman. “He is fine,” replied the old soldier. “He works hard sewing and mending clothes for the saints and angels, but even so, he sometimes doesn’t have enough money to eat.” “And did he send me a message?”, she asked. “Yes, he did! That is why I’ve come here. He asked me to bring back some money for him, so that he can spend his days in paradise more comfortably.” On hearing this, the poor woman’s heart almost broke with pity.',
@@ -25,6 +25,32 @@ List dataAudios = [
       {'name': 'John','path': 'audios/2/john.mp3'},
       {'name': 'Moira','path': 'audios/2/moira.mp3'},
       {'name': 'Natalie','path': 'audios/2/natalie.mp3'},
+    ]
+  },
+  {
+    'id':3,
+    'title': 'Jack Hannaford',
+    'subtitle': 'Parte 03',
+    'en_text': '“I’ll give him anything he wants! He’s a good man, God bless him,” she said. Then she went to the chimney where the couple’s savings were kept hidden. She took out two pieces of gold and five pieces of silver, which in those days was a lot of money. She handed everything over to the old soldier and told him to hurry up and give it to her first husband. “I will,” Jack told the woman, “as soon as I return to Heaven.” When the farmer came back, his wife told him all about how a messenger had come from Heaven. She said that the man asked for money for her first husband, who was living and working in Heaven, but who was poor. “You are a foolish woman!” shouted the farmer. “How could you believe such a silly story?” “Well, you are even more foolish,” said the woman, “because you showed me where the money was hidden.” The husband did not agree with what she said, but he did not stay to argue. He hurried off on his horse to try to find the old soldier before he got too far away.',
+    'pt_text': '“Eu vou dar a ele qualquer coisa que ele quiser! Ele é um bom homem, Deus o abençoe”, ela disse. Então ela foi até a chaminé, onde as economias do casal ficavam escondidas. Ela retirou duas moedas de ouro e cinco moedas de prata, o que naqueles dias era muito dinheiro. Ela entregou tudo para o velho soldado e falou para ele se apressar e entregar o dinheiro para seu primeiro marido. “Eu vou”, Jack falou para a mulher, “assim que eu retornar ao céu”. Quando o fazendeiro voltou, sua esposa contou para ele tudo sobre como um mensageiro tinha vindo do céu. Ela disse que o homem pediu dinheiro para seu primeiro marido, que estava morando e trabalhando no céu, mas que era pobre. “Você é uma mulher tola!”, gritou o fazendeiro. “Como você pode acreditar em uma história tão boba?” “Bom, você é ainda mais tolo”, disse a mulher, “Porque você me mostrou onde o dinheiro estava escondido.” O marido não concordou com o que ela disse, mas ele não ficou para discutir. Ele partiu às pressas em seu cavalo para tentar encontrar o velho soldado antes que ele estivesse muito longe.',
+    'audios': [
+      {'name': 'Jake','path': 'audios/3/jake.mp3'},
+      {'name': 'John','path': 'audios/3/john.mp3'},
+      {'name': 'Moira','path': 'audios/3/moira.mp3'},
+      {'name': 'Natalie','path': 'audios/3/natalie.mp3'},
+    ]
+  },
+  {
+    'id':4,
+    'title': 'Jack Hannaford',
+    'subtitle': 'Parte 04',
+    'en_text': '“Jack Hannaford heard the sound of the horse behind him. He knew that the farmer was coming for him and for the money. Quickly, he came up with a plan. He knelt by the side of the road, covered his eyes with one hand, and pointed up to the sky with the other. The farmer soon reached him and asked: “What are you doing here by the side of the road? Why are you protecting your eyes and pointing up to the sky?” “It is amazing! It is God’s work!” shouted the man. “I see a wonderful thing!” “What kind of wonderful thing?” asked the farmer. “A man is walking straight up to Heaven on a rainbow, just as if it were a road.” The farmer looked at the sky, but he could not see the amazing rainbow or the man walking on it. “Here,” said the soldier, “kneel down by the side of the road and look up at the sky like I’m doing.” “I will if you’ll hold my horse,” said the farmer, and he jumped down from his horse. As soon as the farmer knelt down, Jack Hannaford jumped onto the horse and escaped! Now, who do you think was more foolish, the farmer or his wife?',
+    'pt_text': 'Jack Hannaford ouviu o som do cavalo atrás dele. Ele sabia que o fazendeiro estava vindo atrás dele e do dinheiro. Rapidamente, ele bolou um plano. Ele ajoelhou-se ao lado da estrada, cobriu seus olhos com uma mão, e apontou para o céu com a outra. O fazendeiro logo o alcançou e perguntou: “O que você está fazendo aqui ao lado da estrada? Por que você está protegendo os seus olhos e apontando para o céu?”. “É incrível! É uma obra de Deus!”, gritou o homem. “Eu vejo uma coisa maravilhosa!”. “Que tipo de coisa maravilhosa?”, perguntou o fazendeiro. “Um homem está andando diretamente para o céu em um arco-íris, exatamente como se fosse uma estrada.” O fazendeiro olhou para o céu, mas ele não conseguiu ver o incrível arco-íris ou o homem andando sobre ele. “Aqui,”, disse o soldado, ajoelhe- se ao lado da estrada e olhe para o céu como eu estou fazendo.” “Eu vou se você segurar meu cavalo”, disse o fazendeiro, e ele pulou do seu cavalo. Logo que o fazendeiro ajoelhou-se, Jack Hannaford pulou no cavalo e escapou! Agora, quem você acha que foi mais tolo, o fazendeiro ou sua esposa?',
+    'audios': [
+      {'name': 'Jake','path': 'audios/4/jake.mp3'},
+      {'name': 'John','path': 'audios/4/john.mp3'},
+      {'name': 'Moira','path': 'audios/4/moira.mp3'},
+      {'name': 'Natalie','path': 'audios/4/natalie.mp3'},
     ]
   },
 ];
