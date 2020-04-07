48a6f1986d88d49433ca9604c25c6031704d5270
breno.monte <breno.monte@unimedteresina.com.br>
2020-04-07 16:00:01 -0300

    Changing Sentence listing and Playing in another screen

---
+++ lib/screens/sentence.dart
@@ -0,0 +1,243 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:bm2_text_audio_app/widgets/painter.dart';
+import 'package:audioplayers/audio_cache.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+class Sentence extends StatefulWidget {
+
+  final Map sentence;
+
+  Sentence({Key key, @required this.sentence}) : super(key: key);
+
+  @override
+  _SentenceState createState() => _SentenceState();
+}
+
+class _SentenceState extends State<Sentence> with TickerProviderStateMixin {
+  AnimationController controller;
+  bool isPlaying = false;
+  AudioPlayer advancedPlayer;
+  AudioCache audioCache;
+
+  String get timerString {
+    Duration duration = controller.duration * (controller.value == 0.0 ? 1 : controller.value);
+    return '${duration.inMinutes}:${(duration.inSeconds % 60).toString().padLeft(2, '0')}';
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      vsync: this,
+      duration: Duration(minutes: 10),
+    );
+    controller.addStatusListener((status){
+      if(status == AnimationStatus.dismissed){
+        setState(() {
+          isPlaying = false;
+        });
+      }
+    });
+    initPlayer();
+  }
+
+  @override
+  void dispose() {
+    advancedPlayer.stop();
+    advancedPlayer = null;
+    audioCache = null;
+    super.dispose();
+  }
+
+  void initPlayer(){
+    advancedPlayer = AudioPlayer();
+    audioCache = AudioCache(fixedPlayer: advancedPlayer);
+
+    advancedPlayer.completionHandler = () => setState((){
+      if(isPlaying){
+        _play();
+      }
+    });
+  }
+
+  _play() {
+      audioCache.play(widget.sentence['audio']);
+  }
+
+  _stop() {
+      advancedPlayer.stop();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Sentence"),
+        leading: IconButton(
+            icon: Icon(Icons.arrow_back, color: Colors.white),
+            onPressed: () {
+              _stop();
+              advancedPlayer = null;
+              audioCache = null;
+              Navigator.pop(context);
+            }
+        ),
+        centerTitle: true,
+      ),
+      //backgroundColor: Colors.white,
+      body: AnimatedBuilder(
+          animation: controller,
+          builder: (context, child) {
+            return Padding(
+              padding: EdgeInsets.all(30.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Expanded(
+                    flex: 3,
+                    child: Align(
+                      alignment: FractionalOffset.center,
+                      child: AspectRatio(
+                        aspectRatio: 1.0,
+                        child: Stack(
+                          children: <Widget>[
+                            Positioned.fill(
+                              child: CustomPaint(
+                                  painter: Painter(
+                                    animation: controller,
+                                    backgroundColor: Colors.black12,
+                                    color: Colors.blue,
+                                  )),
+                            ),
+                            Align(
+                              alignment: FractionalOffset.center,
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: <Widget>[
+                                  Text(
+                                    "Timer",
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(fontSize: 20.0,color: Colors.black54),
+                                  ),
+                                  Text(timerString,style: TextStyle(fontSize: 100.0,color: Colors.black54),),
+                                  FlatButton(
+                                    child: Text('RESET',style: TextStyle(fontSize: 20.0,color: Colors.black54),),
+                                    onPressed: (){
+                                      controller.reset();
+                                      _stop();
+                                      setState(() {
+                                        isPlaying = false;
+                                      });
+                                    },
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Align(
+                      alignment: FractionalOffset.center,
+                      child: Column(
+                        children: <Widget>[
+                          Text(widget.sentence['sentence'], style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),textAlign: TextAlign.center,),
+                          Text(widget.sentence['pronunciation'], style: TextStyle(fontSize: 16),textAlign: TextAlign.center,),
+                          Divider(),
+                          Text('here is the meaning')
+                        ],
+                      ),
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      FloatingActionButton(
+                        heroTag: "btn1",
+                        child: Icon(Icons.settings),
+                        onPressed: (){
+                          _showDialogSetup(context);
+                        },
+                      ),
+                      FloatingActionButton.extended(
+                        heroTag: "btn2",
+                        label: Text(isPlaying ? 'Pause' : 'Play'),
+                        icon: Icon(isPlaying ? Icons.pause : Icons.play_arrow),
+                        onPressed: (){
+                          if(controller.isAnimating) {
+                            controller.stop();
+                            _stop();
+                          } else {
+                            controller.reverse(from: controller.value == 0.0 ? 1.0 : controller.value);
+                            _play();
+                          }
+                          setState(() {
+                            isPlaying = !isPlaying;
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            );
+          }),
+    );
+  }
+
+  void _showDialogSetup(BuildContext context){
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Center(
+            child: Text('Setup'),
+          ),
+          content: Container(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  CupertinoTimerPicker(
+                    mode: CupertinoTimerPickerMode.ms,
+                    minuteInterval: 1,
+                    secondInterval: 1,
+                    initialTimerDuration: controller.duration,
+                    onTimerDurationChanged: (Duration changedtimer) {
+                      setState(() {
+                        if(controller != null){
+                          controller.duration = changedtimer;
+                        }
+                      });
+                    },
+                  ),
+                  SizedBox(height: 10.0,),
+                  Divider(),
+                  Row(
+                    children: <Widget>[
+                      Expanded(
+                        flex: 1,
+                        child: FlatButton(
+                          child: Text(
+                            "CLOSE",
+                            style: TextStyle(color: Theme.of(context).primaryColor),
+                          ),
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              )
+          ),
+        );
+      },
+    );
+  }
+}
--- lib/tabs/sentences.dart
+++ lib/tabs/sentences.dart
@@ -1,83 +1,8 @@
-import 'package:audioplayers/audio_cache.dart';
-import 'package:audioplayers/audioplayers.dart';
+import 'package:bm2_text_audio_app/screens/sentence.dart';
 import 'package:flutter/material.dart';
 import 'package:bm2_text_audio_app/data/data.dart';
 
-
-enum PlayerState { stopped, playing }
-
-class Sentences extends StatefulWidget {
-  @override
-  _SentencesState createState() => _SentencesState();
-}
-
-class _SentencesState extends State<Sentences> {
-
-  AudioPlayer advancedPlayer;
-  AudioCache audioCache;
-  PlayerState state;
-  String audio;
-  List sentences = dataSentences;
-
-  @override
-  void initState() {
-    super.initState();
-    sentences = sentences.map((sentence){
-      sentence['playing'] = false;
-      return sentence;
-    }).toList();
-    initPlayer();
-  }
-
-  @override
-  void dispose() {
-    advancedPlayer.stop();
-    advancedPlayer = null;
-    audioCache = null;
-    super.dispose();
-  }
-
-  void initPlayer(){
-    advancedPlayer = AudioPlayer();
-    audioCache = AudioCache(fixedPlayer: advancedPlayer);
-
-    advancedPlayer.completionHandler = () => setState((){
-      if(audio != null){
-        _play(audio);
-      }
-    });
-  }
-
-  _play(String audioPath) {
-    setState(() {
-      if(state == PlayerState.playing){
-        advancedPlayer.stop();
-      }
-      sentences = sentences.map((sentence){
-        if(audioPath == sentence['audio']){
-          sentence['playing'] = true;
-        } else {
-          sentence['playing'] = false;
-        }
-        return sentence;
-      }).toList();
-      audio = audioPath;
-      audioCache.play(audio);
-      state = PlayerState.playing;
-    });
-  }
-
-  _stop(String audioPath) {
-    setState(() {
-      sentences = sentences.map((sentence){
-        sentence['playing'] = false;
-        return sentence;
-      }).toList();
-      audio = null;
-      advancedPlayer.stop();
-      state = PlayerState.stopped;
-    });
-  }
+class Sentences extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
@@ -85,53 +10,25 @@ class _SentencesState extends State<Sentences> {
       child: ListView(
         children: ListTile.divideTiles(
             context: context,
-            tiles: sentences.map((sentence) => _getTile(sentence)).toList()
+            tiles: dataSentences.map((sentence) => _getTile(sentence, context))
+                .toList()
         ).toList(),
       ),
     );
   }
 
-  Widget _getTile(Map sentence){
-    return MyTile(sentence: sentence, playAction: _play, stopAction: _stop);
-  }
-}
-
-
-class MyTile extends StatefulWidget {
-
-  final Map sentence;
-  final ValueChanged<String> playAction;
-  final ValueChanged<String> stopAction;
-
-  MyTile({Key key, @required this.sentence, @required this.playAction, @required this.stopAction}) : super(key: key);
-
-  @override
-  _MyTaleState createState() => _MyTaleState(sentence);
-}
-
-class _MyTaleState extends State<MyTile> {
-
-  final Map sentence;
-
-  _MyTaleState(this.sentence);
-
-  @override
-  Widget build(BuildContext context) {
+  Widget _getTile(Map sentence, BuildContext context) {
     return ListTile(
-      leading: IconButton(
-        onPressed: (){
-          setState(() {
-            if(sentence['playing']){
-              widget.stopAction(sentence['audio']);
-            } else {
-              widget.playAction(sentence['audio']);
-            }
-          });
-        },
-        icon: Icon(sentence['playing'] ? Icons.stop : Icons.play_arrow, color: sentence['playing'] ? Colors.blue[300] : Colors.black54),
-      ),
-      title: Text(sentence['sentence'], style: TextStyle(color: sentence['playing'] ? Colors.blue : Colors.black),),
-      subtitle: Text(sentence['pronunciation'], style: TextStyle(color: sentence['playing'] ? Colors.blue[300] : Colors.black54),),
+      title: Text(sentence['sentence']),
+      subtitle: Text(sentence['pronunciation']),
+      trailing: Icon(Icons.arrow_forward_ios),
+      onTap: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => Sentence(sentence: sentence),
+            ));
+      },
     );
   }
 }
